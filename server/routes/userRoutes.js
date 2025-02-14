9d0509685cb4b9e9de623ea99be3d28c3357f25c
murichu <murichuchichi@gmail.com>
2025-02-14 15:23:52 +0000

    updated server

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import userAuth from '../middleware/userAuth.js';
+//import userAuth from '../middleware/userAuth.js';
 import { getUserData } from '../controllers/userController.js';
 
 
 const userRouter = express.Router();
 
-userRouter.get('/data', userAuth, getUserData);
+userRouter.get('/data', getUserData);
 
 export default userRouter;
 
