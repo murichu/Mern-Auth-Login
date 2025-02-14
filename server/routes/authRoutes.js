186c21e9e1a1aa5fa13e58ca6148b92b399a2509
Jacob Murichu <77874248+murichu@users.noreply.github.com>
2025-02-14 19:19:47 +0300

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { register, login, logout, verifyEmail, sendVerifyOtp,sendResetOtp, resetPassword, isAuthenticated } from '../controllers/authController.js';
-//import userAuth from '../middleware/userAuth.js';
+import userAuth from '../middleware/userAuth.js';
 
 
 const authRouter = express.Router();
@@ -8,9 +8,9 @@ const authRouter = express.Router();
 authRouter.post('/register', register);
 authRouter.post('/login', login);
 authRouter.post('/logout', logout);
-authRouter.post('/send-verify-otp', sendVerifyOtp);
-authRouter.post('/verify-account', verifyEmail);
-authRouter.get('/is-auth', isAuthenticated);
+authRouter.post('/send-verify-otp', userAuth, sendVerifyOtp);
+authRouter.post('/verify-account', userAuth, verifyEmail);
+authRouter.get('/is-auth', userAuth, isAuthenticated);
 authRouter.post('/send-reset-otp',  sendResetOtp);
 authRouter.post('/reset-password',  resetPassword);
 
